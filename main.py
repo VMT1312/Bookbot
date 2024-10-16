0fbc86c353da14fa8411c6a12bf7650b9240ece4
VMT1312 <vinhthiem.ma@curtin.edu.au>
2024-10-16 14:07:28 +0800

    Print the number of character

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ def get_num_words(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+    
+def get_num_chars(text):
+    char_dict = {}
+    for word in text:
+        lowered_word = word.lower()
+        if lowered_word in char_dict:
+            char_dict[lowered_word] +=1
+        else:
+            char_dict[lowered_word] = 1
+    return char_dict
 
 book_path = "books/frankenstein.txt"
 
@@ -13,6 +23,8 @@ text = get_book_text(book_path)
 
 num_words = get_num_words(text)
 
+chars_num = get_num_chars(text)
+
 print(f"{num_words} words found in the document")
 
-
+print(chars_num)
