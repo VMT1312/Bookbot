3ca78ee67aa24c89ca159da9728e1843195a8c31
VMT1312 <vinhthiem.ma@curtin.edu.au>
2024-10-16 13:30:33 +0800

    My Style

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
-
-
 def get_num_words(text):
     words = text.split()
     return len(words)
@@ -14,6 +7,12 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+book_path = "books/frankenstein.txt"
 
-main()
+text = get_book_text(book_path)
+
+num_words = get_num_words(text)
+
+print(f"{num_words} words found in the document")
+
 
